f5c8a0a0572d3f795ba8087dbaae9839d2eb077f
TheRori <37111863+TheRori@users.noreply.github.com>
2023-05-29 16:15:13 +0200

    Add Venn diagrams

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # This is a sample Python script.
 import logging
 import pandas as pd
+import numpy as np
 from pathlib import Path
 import matplotlib.pyplot as plt
 from unidecode import unidecode
+from matplotlib_venn import venn2, venn3
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from tagtog2df.tagtog2df import allfiles_onedataframe
@@ -88,23 +90,50 @@ def add_emotions_pyfeel(df):
         emotions.append(max_em)
     df['PyFeel'] = emotions
 
+def make_diagram(df,em1,em2,em3):
+    inter1 = df[(df['TagTog'] == em1) & (df['PyFeel'] == em2)]
+    inter2 = df[(df['Senticnet'] == em3) & (df['PyFeel'] == em2)]
+    inter3 = df[(df['Senticnet'] == em3) & (df['TagTog'] == em1)]
+    inter4 = df[(df['Senticnet'] == em3) & (df['TagTog'] == em1) & (df['PyFeel'] == em2)]
+
+    mask2 = df[df['TagTog'] == em1]
+    mask3 = df[df['PyFeel'] == em2]
+    mask4 = df[df['Senticnet'] == em3]
+
+    df_similarity = pd.DataFrame(np.array([[em1,(len(mask2)),(len(mask3)),(len(mask4)),(len(inter1)),(len(inter3)),(len(inter2)),(len(inter4))]]),
+                 columns=['Emotion','TagTog', 'PyFeel', 'Senticnet','T-P','T-S','P-S','T-P-S'])
+
+    venn3(subsets=(len(mask2),len(mask3), len(mask4), len(inter1), len(inter2), len(inter3), len(inter4)),
+          set_labels=('TagTog', 'PyFeel', 'Senticnet'))
+    plt.title(em1)
+    plt.show()
+    return df_similarity
+
 
 if __name__ == '__main__':
 
     #df = pd.read_csv('csv/all_df_clean.csv')
 
-    df = make_df()
-    add_emotions_senticnet(df)
+    #df = make_df()
+    #add_emotions_senticnet(df)
 
-    add_emotions_pyfeel(df)
-    df.to_csv('csv/emotions_df.csv')
+    #add_emotions_pyfeel(df)
+    #df.to_csv('csv/emotions_df.csv')
     df = pd.read_csv('csv/emotions_df.csv')
-    df['TagTog'].value_counts().plot(kind='bar')
-    plt.show()
-    df['Senticnet'].value_counts().plot(kind='bar')
-    plt.show()
-    df['PyFeel'].value_counts().plot(kind='bar')
-    plt.show()
+    df2 = make_diagram(df,'Fear','fear','grief')
+    df3 = make_diagram(df,'Calmness','calmness','grief')
+    df4 = make_diagram(df,'Joy','joy','serenity')
+    dfc= pd.concat([df2, df3], axis=0)
+    dfc= pd.concat([dfc, df4], axis=0)
+
+
+    print(dfc.head())
+    #df['TagTog'].value_counts().plot(kind='bar')
+    #plt.show()
+    #df['Senticnet'].value_counts().plot(kind='bar')
+    #plt.show()
+    #df['PyFeel'].value_counts().plot(kind='bar')
+    #plt.show()
     # print(df.TagTog.unique())
     # print(df.Senticnet.unique())
     # print(df.PyFeel.unique())
